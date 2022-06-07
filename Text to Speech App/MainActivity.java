e729521f666aeb71c2d72fa9108aa191f6ba0748
Rishabh122333 <102612221+Rishabh122333@users.noreply.github.com>
2022-06-08 02:25:18 +0530

    Text to Speech app code
    This app speaks the  message which is entered by the user

---
+++ Text to Speech App/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.acer.texttoseech;
+
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+    TextToSpeech ts;
+    EditText e1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.button);
+        e1=(EditText)findViewById(R.id.editText);
+        ts=new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                ts.setLanguage(Locale.ENGLISH);
+                ts.setSpeechRate(1.0f);
+            }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s1=e1.getText().toString();
+                ts.speak(s1,TextToSpeech.QUEUE_FLUSH,null);
+            }
+        });
+
+    }
+}
